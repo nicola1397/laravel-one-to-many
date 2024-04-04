48eac3805a37500548dff15acd0e3b71bc3ee025
nicola1397 <nicola1397@outlook.it>
2024-04-04 17:17:41 +0200

    first commit

---
+++ database/seeders/ProjectSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Project;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Str;
+
+class ProjectSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $project = new Project;
+        $project->title;
+        $project->slug = Str::slug($project->title);
+        $project->category;
+        $project->description;
+        $project->save();
+    }
+}
---
+++ resources/views/admin/projects/create.blade.php
@@ -0,0 +1,49 @@
+@extends('layouts.app')
+
+@section('title', 'Nuovo Progetto')
+
+@section('content')
+    <div class="container">
+        <a href="{{ route('admin.projects.index') }}" class="btn btn-primary mt-4 mb-3">Torna alla lista</a>
+
+        <h1 class="mb-3">Crea un nuovo progetto</h1>
+
+        <form action="{{ route('admin.projects.store') }}" method="POST">
+            @csrf
+
+            <div class="row g-3">
+                <div class="col-6">
+                    <label for="title" class="form-label">Titolo</label>
+                    <input type="text" class="form-control @error('title') is-invalid @enderror" id="title"
+                        name="title" value="{{ old('title') }}">
+                    @error('title')
+                        <div class="invalid-feedback">{{ $message }}</div>
+                    @enderror
+                </div>
+<!-- CATEGORIA! -->
+                <div class="col-6">
+                    <label for="category" class="form-label">Categoria</label>
+                    <input type="text" class="form-control @error('category') is-invalid @enderror" id="category"
+                        name="category" value="{{ old('category')}}">
+
+                    @error('category')
+                        <div class="invalid-feedback">{{ $message }}</div>
+                    @enderror
+                </div>
+
+                <div class="col-12">
+                    <label for="description" class="form-label">Descrizione</label>
+                    <textarea class="form-control @error('description') is-invalid @enderror" id="description" name="description"
+                        rows="3">{{ old('description') }}</textarea>
+                    @error('description')
+                        <div class="invalid-feedback">{{ $message }}</div>
+                    @enderror
+                </div>
+
+                <div class="col-2">
+                    <button class="btn btn-success">Salva</button>
+                </div>
+            </div>
+        </form>
+    </div>
+@endsection
